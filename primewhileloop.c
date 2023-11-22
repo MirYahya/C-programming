42ea9235d56b63ea681ba1a8603d8a43e732c034
Mir Yahya Bin Shafi <146697999+MirYahya@users.noreply.github.com>
2023-11-22 16:52:31 +0530

    Add files via upload

---
+++ primewhileloop.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int num,i,c=0;
+    printf("input a number");
+    scanf("%d",&num);
+    for(i=2;i<=num/2;i++)
+    {
+        while (num%i==0)
+        {
+            c++;
+            break;
+        } 
+    }
+    if (c==0 && num!=1)
+    {
+    printf("%d is a prime number  .\n",num);
+    }
+    else
+    {
+        printf("%d is not a prime number",num);
+    }
+}
+
+    
+    
