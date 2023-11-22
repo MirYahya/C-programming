a7839af6afc76caa1dcce964b425a5896f820603
Mir Yahya Bin Shafi <146697999+MirYahya@users.noreply.github.com>
2023-11-22 16:11:18 +0530

    Add files via upload

---
+++ arithematic.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c,d,e,f;
+    printf("enter two numbers");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    d=a-b;
+    e=a*b;
+    f=a/b;
+    printf("sum=%d\n",c);
+    printf("difference=%d\n",d);
+    printf("product=%d\n",e);
+    printf("division=%d\n",f);
+}
+
+
+
+
+
